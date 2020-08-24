81529f62a63f3555995e2a677a7656d9c9a5ac87
Louis Fialho <louis.fialho@hec.edu>
2020-08-24 17:59:12 +0100

    finished flats model

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,4 +7,20 @@ class FlatsController < ApplicationController
    def show
     @flat = Flat.find(params[:id])
   end
+
+   def new
+    @flat = Flat.new # needed to instantiate the form_for
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :description, :location, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   get "flats", to: "flats#index"
 
+  get "flats/new", to: "flats#new"
+
   get "flats/:id", to: "flats#show", as: :flat
+
+  post "flats", to: "flats#create"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
