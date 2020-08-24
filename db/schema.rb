000dbecb979821423a6be551c248e0ab39c45b8f
Louis Fialho <louis.fialho@hec.edu>
2020-08-24 17:29:51 +0100

    Corrected typo: lcation => location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_160651) do
+ActiveRecord::Schema.define(version: 2020_08_24_162845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_160651) do
   create_table "flats", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.string "lcation"
+    t.string "location"
     t.integer "price"
     t.integer "average_review"
     t.datetime "created_at", precision: 6, null: false
