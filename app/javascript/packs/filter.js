f6a15e6d500921c0464eefe3c265636c07cc0934
Louis Fialho <louis.fialho@hec.edu>
2020-08-27 17:03:05 +0100

    Added basic version of type filtering

--- app/javascript/packs/filter.js
+++ app/javascript/packs/filter.js
@@ -1,14 +1,17 @@
 
 
-export function filter() {
+export function price_filter() {
   const filter = document.getElementById("price-filter");
 
   filter.addEventListener("click", (event) => {
-    // display a white box with min price, max price
     const card = document.getElementById("price-card");
-    card.style.display = "";
+      if (card.style.display === "none") {
+        card.style.display = "";
+      } else {
+        card.style.display = "none";
+      }
 
-    const save = document.getElementById("save");
+    const save = document.getElementById("save-price");
       save.addEventListener("click", (event) => {
         card.style.display = "none";
 
@@ -25,10 +28,48 @@ export function filter() {
         });
 
       });
-
-    // 2) Actualise le bouton price avec les nvelles valeurs
   });
 };
 
+export function type_filter() {
+  const filter = document.getElementById("type-filter");
+  filter.addEventListener("click", (event) => {
+    const card = document.getElementById("type-card");
+    if (card.style.display === "none") {
+        card.style.display = "";
+    } else {
+        card.style.display = "none";
+    }
 
+    const save = document.getElementById("save-type");
+
+    let preferences = []
+
+    save.addEventListener("click", (event) => {
+        card.style.display = "none";
+        if (document.getElementById("defaultCheck1").checked == true) {
+          preferences.push(document.getElementById("defaultText1").innerHTML);
+        }
+        if (document.getElementById("defaultCheck2").checked == true) {
+          preferences.push(document.getElementById("defaultText2").innerHTML);
+        }
+        if (document.getElementById("defaultCheck3").checked == true) {
+          preferences.push(document.getElementById("defaultText3").innerHTML);
+        }
+        if (document.getElementById("defaultCheck4").checked == true) {
+          preferences.push(document.getElementById("defaultText4").innerHTML);
+        }
+
+          document.querySelectorAll(".flat").forEach((flat) => {
+
+          if (preferences.includes(flat.dataset.type) == false) {
+            flat.style.display = "none";
+          }
+
+          filter.innerHTML = `Type (${preferences.length})`;
+
+        });
+  });
+});
+};
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@ require 'faker'
 
 puts "Creating flats..."
 
+type = ["full accommodation", "private room", "hotel room", "shared room" ]
+
+
+
+
 sean = User.new(name:"Sean", role:"visitor", email:"a@gmail.com", password:"abc123")
 sean.save!
 louis = User.new(name:"Louis", role:"host", email:"l@gmail.com", password:"abc123")
@@ -14,61 +19,39 @@ ricardo = User.new(name:"Ricardo", role:"host", email:"123@gmail.com", password:
 louis.save!
 
 30.times do
-  flat = Flat.new(host:louis, title: Faker::Address.street_name, description: "Really nice flat in #{:location}", location: "London", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
+  flat = Flat.new(host:louis, flat_type: type.sample,title: Faker::Address.street_name, description: "Really nice flat in #{:location}", location: "London", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
   flat.save!
 end
 
 30.times do
-  flat = Flat.new(host:louis, title: Faker::Address.street_name, description: "Really nice flat in #{:location}", location: "Paris", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
+  flat = Flat.new(host:louis, flat_type: type.sample, title: Faker::Address.street_name, description: "Really nice flat in #{:location}", location: "Paris", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
   flat.save!
 end
 
-flat = Flat.new(host:louis, title: "London Big flat", description: "Really nice flat in Marques de Pombal", location: "London", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
+flat = Flat.new(host:louis, flat_type: type.sample, title: "London Big flat", description: "Really nice flat in Marques de Pombal", location: "London", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
 flat.save!
 
 
-flat = Flat.new(host:louis, title: "London Big flat", description: "Really nice flat in Marques de Pombal", location: "London", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
+flat = Flat.new(host:louis, flat_type: type.sample, title: "London Big flat", description: "Really nice flat in Marques de Pombal", location: "London", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
  flat.save!
 
-flat = Flat.new(host:louis, title: "Bairro Alto flat", description: "Really nice flat in Bairro Alto", location: "Lisbon", price: rand(100.1000), image_url: "https://d1bvpoagx8hqbg.cloudfront.net/originals/amazing-view-lisbon-centre-34e852280371ff86cefa5b457c1dcb85.jpg", capacity: rand(1..5))
+flat = Flat.new(host:louis, flat_type: type.sample, title: "Bairro Alto flat", description: "Really nice flat in Bairro Alto", location: "Lisbon", price: rand(100.1000), image_url: "https://d1bvpoagx8hqbg.cloudfront.net/originals/amazing-view-lisbon-centre-34e852280371ff86cefa5b457c1dcb85.jpg", capacity: rand(1..5))
 flat.save!
 
- flat = Flat.new(host:louis, title: "Belem flat", description: "Really nice flat in Belem", location: "Lisbon", price: rand(100.1000), image_url:"https://thespaces.com/wp-content/uploads/2017/01/Lisbon-apartments-for-rent-Baixa-House-1.jpg", capacity: rand(1..5))
+ flat = Flat.new(host:louis, flat_type: type.sample, title: "Belem flat", description: "Really nice flat in Belem", location: "Lisbon", price: rand(100.1000), image_url:"https://thespaces.com/wp-content/uploads/2017/01/Lisbon-apartments-for-rent-Baixa-House-1.jpg", capacity: rand(1..5))
  flat.save!
 
- flat = Flat.new(host:louis, title: "Chiado flat", description: "Really nice flat in Chiado", location: "Lisbon", price: rand(100.1000), image_url:"https://d1bvpoagx8hqbg.cloudfront.net/originals/sunny-flat-heart-lisbon-207180cf4f84f05e668755d0f82ee94b.jpg", capacity: rand(1..5))
+ flat = Flat.new(host:louis, flat_type: type.sample, title: "Chiado flat", description: "Really nice flat in Chiado", location: "Lisbon", price: rand(100.1000), image_url:"https://d1bvpoagx8hqbg.cloudfront.net/originals/sunny-flat-heart-lisbon-207180cf4f84f05e668755d0f82ee94b.jpg", capacity: rand(1..5))
  flat.save!
 
- flat = Flat.new(host:louis, title: "Principe Real flat", description: "Really nice flat in Principe Real", location: "Lisbon", price: rand(100.1000), image_url:"https://thespaces.com/wp-content/uploads/2017/01/Lisbon-apartments-for-rent-Baixa-House.jpg", capacity: rand(1..5))
+ flat = Flat.new(host:louis, flat_type: type.sample, title: "Principe Real flat", description: "Really nice flat in Principe Real", location: "Lisbon", price: rand(100.1000), image_url:"https://thespaces.com/wp-content/uploads/2017/01/Lisbon-apartments-for-rent-Baixa-House.jpg", capacity: rand(1..5))
  flat.save!
 
- flat = Flat.new(host:louis, title: "Baixa flat", description: "Really nice flat in Baixa", location: "Lisbon", price: rand(100.1000), image_url: "https://a0.muscache.com/im/pictures/29be0469-56ab-456d-baeb-52cb13b81ad8.jpg?aki_policy=xx_large", capacity: rand(1..5))
+ flat = Flat.new(host:louis, flat_type: type.sample, title: "Baixa flat", description: "Really nice flat in Baixa", location: "Lisbon", price: rand(100.1000), image_url: "https://a0.muscache.com/im/pictures/29be0469-56ab-456d-baeb-52cb13b81ad8.jpg?aki_policy=xx_large", capacity: rand(1..5))
  flat.save!
 
- flat = Flat.new(host:louis, title: "Marques de Pombal flat", description: "Really nice flat in Marques de Pombal", location: "Lisbon", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
+ flat = Flat.new(host:louis, flat_type: type.sample, title: "Marques de Pombal flat", description: "Really nice flat in Marques de Pombal", location: "Lisbon", price: rand(100.1000), image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
 
-
- bairro = Flat.new(host:louis, title: "Bairro Alto flat", description: "Really nice flat in Bairro Alto", location: "Lisbon", price: 100, image_url: "https://d1bvpoagx8hqbg.cloudfront.net/originals/amazing-view-lisbon-centre-34e852280371ff86cefa5b457c1dcb85.jpg", capacity: rand(1..5) )
- bairro.save!
-
- belem = Flat.new(host:louis, title: "Belem flat", description: "Really nice flat in Belem", location: "Lisbon", price: 100, image_url:"https://thespaces.com/wp-content/uploads/2017/01/Lisbon-apartments-for-rent-Baixa-House-1.jpg", capacity: rand(1..5))
- belem.save!
-
- chiado = Flat.new(host:louis, title: "Chiado flat", description: "Really nice flat in Chiado", location: "Lisbon", price: 100, image_url:"https://d1bvpoagx8hqbg.cloudfront.net/originals/sunny-flat-heart-lisbon-207180cf4f84f05e668755d0f82ee94b.jpg", capacity: rand(1..5))
- chiado.save!
-
-
-flat = Flat.new(host:louis, title: "Principe Real flat", description: "Really nice flat in Principe Real", location: "Lisbon", price: 100, image_url:"https://thespaces.com/wp-content/uploads/2017/01/Lisbon-apartments-for-rent-Baixa-House.jpg", capacity: rand(1..5))
-flat.save!
-
-flat = Flat.new(host:louis, title: "Baixa flat", description: "Really nice flat in Baixa", location: "Lisbon", price: 100, image_url: "https://a0.muscache.com/im/pictures/29be0469-56ab-456d-baeb-52cb13b81ad8.jpg?aki_policy=xx_large", capacity: rand(1..5))
-flat.save!
-
-flat = Flat.new(host:louis, title: "Marques de Pombal flat", description: "Really nice flat in Marques de Pombal", location: "Lisbon", price: 100, image_url: "https://dqav65i4xqidd.cloudfront.net/app/uploads/20180519094708/t2-sete-rios-2.png", capacity: rand(1..5))
-flat.save!
-
-# review1 = Review.new(text:"amazing stunning flat", user:sean, flat:bairro)
-# review1.save
 
 puts 'Finished!'
 
