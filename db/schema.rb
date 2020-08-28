9b0a655c02e72523cfd046f70ff5c81ff63dc74d
Christopher Nitsche <christopher.nitsche@gmx.de>
2020-08-28 14:38:37 +0100

    Random Images done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_164416) do
+ActiveRecord::Schema.define(version: 2020_08_28_130631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,11 +35,13 @@ ActiveRecord::Schema.define(version: 2020_08_27_164416) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "host_id", null: false
     t.string "image_url"
-    t.integer "capacity"
-    t.string "flat_type"
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.integer "capacity"
+    t.string "flat_type"
+    t.string "image_url2"
+    t.string "image_url3"
     t.index ["host_id"], name: "index_flats_on_host_id"
   end
 
@@ -52,12 +54,6 @@ ActiveRecord::Schema.define(version: 2020_08_27_164416) do
     t.bigint "flat_id"
     t.index ["flat_id"], name: "index_reviews_on_flat_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
-  create_table "specs", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
